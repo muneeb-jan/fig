baf42474de1e2d40c12bd6f3d80ead9ab64ce071
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2021-12-13 19:17:29 +0100

    Always inject faults

--- faultinjectors/fi_write_EIO.c
+++ faultinjectors/fi_write_EIO.c
@@ -9,15 +9,15 @@
 typedef ssize_t (*write_type) (int fd, const void *buf, size_t count);
 ssize_t write (int fd, const void *buf, size_t count) {
     static write_type orig = 0;
-    if (drand48 () < 0.1) {
+    //if (drand48 () < 0.1) {
         errno = EIO;
         return -1;
-    }
-    if (0 == orig) {
+    //}
+    /*if (0 == orig) {
         orig = (write_type)dlsym (RTLD_NEXT, "write");
         assert (orig && "original write function not found");
     }
-    return orig (fd, buf, count);
+    return orig (fd, buf, count);*/
 }
 
  
