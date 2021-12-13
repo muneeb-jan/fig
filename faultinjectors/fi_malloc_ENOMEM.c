04247a06221e25221a651e0bf073a59636545a2d
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2021-12-13 19:20:06 +0100

    Always inject faults

--- faultinjectors/fi_malloc_ENOMEM.c
+++ faultinjectors/fi_malloc_ENOMEM.c
@@ -11,13 +11,13 @@
 typedef void* (*malloc_type) (size_t size);
 void *malloc(size_t size){
     static malloc_type orig = 0;
-    if (drand48 () < 0.1) {
+    //if (drand48 () < 0.1) {
         errno = ENOMEM;
         return -1;
-    }
-    if (0 == orig) {
+    //}
+    /*if (0 == orig) {
         orig = (malloc_type)dlsym (RTLD_NEXT, "malloc");
         assert (orig && "original malloc function not found");
     }
-    return orig (size);
+    return orig (size);*/
 }
