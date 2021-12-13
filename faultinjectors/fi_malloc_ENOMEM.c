75c9c1a1ba19daa88770c38d74b4e3e6e9c01229
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2021-12-13 17:50:06 +0100

    Fault injectors  done

--- faultinjectors/fi_malloc_ENOMEM.c
+++ faultinjectors/fi_malloc_ENOMEM.c
@@ -1,1 +1,23 @@
 // TODO: malloc wrapper
+// TODO: write wrapper
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+
+
+typedef void* (*malloc_type) (size_t size);
+void *malloc(size_t size){
+    static malloc_type orig = 0;
+    if (drand48 () < 0.1) {
+        errno = ENOMEM;
+        return -1;
+    }
+    if (0 == orig) {
+        orig = (malloc_type)dlsym (RTLD_NEXT, "malloc");
+        assert (orig && "original malloc function not found");
+    }
+    return orig (size);
+}
--- faultinjectors/fi_select_ENOMEM.c
+++ faultinjectors/fi_select_ENOMEM.c
@@ -1,1 +1,23 @@
-// TODO: select wrapper
+// TODO: select wrapper// TODO: malloc wrapper
+// TODO: write wrapper
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/select.h>
+
+typedef int (*select_type) (int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict exceptfds, struct timeval *restrict timeout);
+int select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict exceptfds, struct timeval *restrict timeout){
+    static select_type orig = 0;
+    if (drand48 () < 0.1) {
+        errno = ENOMEM;
+        return -1;
+    }
+    if (0 == orig) {
+        orig = (select_type)dlsym (RTLD_NEXT, "select");
+        assert (orig && "original select function not found");
+    }
+    return orig (nfds, readfds, writefds, exceptfds, timeout);
+}
